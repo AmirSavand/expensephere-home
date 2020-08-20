852c7960bb6f3cf1092ca198fb23ea0ae60c9126
Amir Savand <amir@savandbros.com>
2020-08-20 09:47:51 +0300

    Move Google Analytics from HTML to JS

--- src/asset/script.js
+++ src/asset/script.js
@@ -1,3 +1,28 @@
+/**
+ * Initiate bootstrap.native
+ */
 window.addEventListener("load", () => {
   window.BSN.initCallback();
+});
+
+/**
+ * Initiate google analytics
+ */
+window.addEventListener("load", () => {
+  if (location.host.includes("localhost")) {
+    return;
+  }
+  const script = document.createElement("script");
+  script.src = "https://www.googletagmanager.com/gtag/js?id=UA-175669174-2";
+  script.onload = function () {
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag("js", new Date());
+    gtag("config", "UA-175669174-2", {
+      page_title: "Landing Page",
+    });
+  };
+  document.body.append(script);
 });
