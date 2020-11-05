638e65293f19c39f366c33741752a542bc5453b9
Arsalan Savand <arsalan@savandbros.com>
2020-11-05 22:07:44 +0300

    Add script to add animation to download buttons

--- src/asset/script.js
+++ src/asset/script.js
@@ -9,6 +9,35 @@ window.addEventListener("load", () => {
  * Initiate google analytics
  */
 window.addEventListener("load", () => {
+  function map(x, inMin, inMax, outMin, outMax) {
+    return (x - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+  }
+  /**
+   * Add animation to OS download buttons.
+   */
+  document.querySelectorAll(".os").forEach(function (os) {
+    const inner = os.querySelector(".inner");
+
+    os.addEventListener("mousemove", function (e) {
+      const x = e.clientX - this.getBoundingClientRect().x;
+      const y = e.clientY - this.getBoundingClientRect().y;
+
+      const rotateY = map(x, 0, this.offsetWidth, -17, 17);
+      const rotateX = map(y, 0, this.offsetHeight, -17, 17);
+
+      inner.style.transform = "rotateY(" + rotateY + "deg)" + " " + "rotateX(" + -rotateX + "deg)";
+    });
+    os.addEventListener("mouseenter", function () {
+      inner.style.transition = "all " + 0.05 + "s" + " linear";
+      inner.classList.add("hovered");
+    });
+    os.addEventListener("mouseleave", function () {
+      inner.style.transition = "all " + 0.2 + "s" + " linear";
+      inner.classList.remove("hovered");
+      inner.style.transform = "rotateY(" + 0 + "deg)" + " " + "rotateX(" + 0 + "deg)";
+    });
+  });
+
   if (location.host.includes("localhost")) {
     return;
   }
